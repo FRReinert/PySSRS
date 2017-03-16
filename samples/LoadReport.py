c354081cb456cf235f8011a817aaaa3b4a16be47
Fabricio Roberto Reinert <fabricio.reinert@live.com>
2017-03-16 09:25:01 -0300

    [Update] Finished Report Rendering Feature

--- samples/LoadReport.py
+++ samples/LoadReport.py
@@ -8,24 +8,54 @@ from SSRS import SSRS
 
 RS = SSRS(d._service, d._execution, d._user, d._psw)
 
-Parameters = {
-    'Transp': -1,
-    'Frete' : 'C' 
+'''
+Part 1
+    This Section will load the report and it's data
+    you can get parameters data, page size, and every info from it
+'''
+try:
+    Report = RS.RequestReport(path='/Faturamento/Notas por Transportadora')
+
+except BaseException as e:
+    print('Error loadint report: %s' % str(e))
+
+
+'''
+Part 2
+    This section will use the pre-loaded data to render the report
+'''
+try:
+    # Put the parameters into a dictionary
+    Parameters = {
+        'Transp': -1,
+        'Frete' : 'C' 
     }
-
-Report = RS.RequestReport(path='/Faturamento/Notas por Transportadora', format='PDF', parameters=Parameters)
-
-if Report:
-   
-    for k, v in Report.items():
-        if k == 'Result':
-            pass
-        
-        else:
-            print(k,': ',v)
     
-    filename = os.path.join(os.path.dirname(__file__),'report'+ '.' + Report['Extension'])
-    fopen = open(filename, 'wb')
-    fopen.write(Report['Result'])
+    RenderedReport = RS.RenderReport(LoadedReport=Report, format='PDF', parameters=Parameters)
+
+except BaseException as e:
+    print('Error rendering report: %s' % str(e))
+
+
+# This block will return all the information that SSRS can handle
+for k, v in RenderedReport.items():
+    if k == 'Result':
+        pass
+    
+    else:
+        print(k,': ',v)
+
+'''
+This block will render the report into a file
+But you can use it in Django/Flask Request, for example...
+    
+    <> IMPORTANT - If you want to render a file <>
+    Compiled files like PDF, Word, Excel should use "wb" on the file opening
+    Text based files (xml, html...) are "OK" to use the "w" mode 
+'''
+        
+filename = os.path.join(os.path.dirname(__file__),'report'+ '.' + RenderedReport['Extension'])
+fopen = open(filename, 'wb')
+fopen.write(RenderedReport['Result'])
     
     
