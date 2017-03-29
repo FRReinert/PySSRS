48c5ef66ea372637e59f748ed2de1131da9a7823
Fabricio Roberto Reinert <fabricio.reinert@live.com>
2017-03-29 08:04:32 -0300

    [Update] web server implementation
    - Class created
    - Server testing
    - KeyboardInterrupt exception (Not 100%)

--- SSRS/webserver.py
+++ SSRS/webserver.py
@@ -3,55 +3,42 @@
 import socket
 import sys
 
-def usage():
-    msg = '''The following parameters are available:
-        -> runserver'''
-    print(msg)
-    sys.exit(0)
+class http:
+    
+    def __init__(self, host='', port=8888):
+        
+        self.runserver(host, port)        
+    
+    def usage(self):
+        msg = '''The following parameters are available:
+            -> runserver'''
+        print(msg)
+        sys.exit(0)
+    
+    def runserver(self, HOST,PORT):
+        '''
+            Server implementation
+        '''
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listen_socket.bind((HOST, PORT))
+        listen_socket.listen(1)
+        print('Serving HTTP on port %s ...' % PORT)
+        
+        while True:
+            
+            try:
+                client_connection, client_address = listen_socket.accept()
+                request = client_connection.recv(1024)
+                print(request)
+                
+                http_response = b"""Hello, World!"""
+                client_connection.sendall(http_response)
+                client_connection.close()
+            
+            except (KeyboardInterrupt, SystemExit):
+                print('interrupted!')
+                return
+            
 
-def runserver(HOST='',PORT=8888):
-    '''
-        Server implementation
-    '''
-    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listen_socket.bind((HOST, PORT))
-    listen_socket.listen(1)
-    print('Serving HTTP on port %s ...' % PORT)
-    while True:
-        client_connection, client_address = listen_socket.accept()
-        request = client_connection.recv(1024)
-        print(request)
-    
-        http_response = """\
-    HTTP/1.1 200 OK
-    
-    Hello, World!
-    """
-        client_connection.sendall(http_response)
-        client_connection.close()
-        
-        
-args = sys.argv
-
-# No parameter specified
-if len(args) == 1:
-    usage()
-
-# Check for runserver param    
-if 'runserver' in args:
-    p_index = args.index('runserver', )
-    
-    # Check if Host, Port was informed
-    if p_index < len(args):
-        
-        # check if the informed host and port is a tuple
-        if isinstance(args[p_index], tuple):
-            runserver(args[p_index])
-        
-        else:
-            raise BaseException("Specify the host and port as a tuple")
-    
-    else:
-        
-        runserver()
+server = http()
